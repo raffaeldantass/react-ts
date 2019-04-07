4d6128e257d3abd19ca2ab37cb2f3e3c10c7511f
Rafael Dantas <rafael.dantas.br@gmail.com>
2019-04-07 15:55:14 -0300

    first stage

---
+++ tsreact/src/store/ducks/repositories/sagas.ts
@@ -0,0 +1,14 @@
+import { call, put } from 'redux-saga/effects';
+import api from '../../../services/api';
+
+import { loadSucess, loadFailure } from './actions';
+
+export function* load() {
+  try {
+    const response = yield call(api.get, 'users/diego3g/repos');
+
+    yield put(loadSucess(response.data));
+  } catch (err) {
+    yield put(loadFailure());
+  }
+}
